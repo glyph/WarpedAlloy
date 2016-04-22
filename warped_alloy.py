57b72a833b595ca29c0e4747e3c70dc74b2ed652
Glyph <glyph@twistedmatrix.com>
2016-04-22 14:54:52 -0700

    more code, all still totally non-functional

--- warped_alloy.py
+++ warped_alloy.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import, print_function
+
+print("FIRST")
+
 import __main__
 
 import sys
 import os
 
-from socket import socketpair
+from socket import socketpair, AF_INET, AF_UNIX, SOCK_STREAM, fromfd
 
 import attr
 
 from twisted.python.usage import Options
 from twisted.internet.task import react
-from twisted.internet.address import UNIXAddress
 from twisted.internet.defer import Deferred, inlineCallbacks
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet.protocol import Protocol, Factory, ProcessProtocol
+from twisted.protocols.amp import AMP, Command, Descriptor
+from twisted.internet.unix import Server as UNIXServer
+
+from twisted.web.server import Site
+from twisted.web.static import Data
 
 class ManagerServerNotReallyProtocol(Protocol, object):
     """
@@ -31,8 +39,9 @@ class ManagerServerNotReallyProtocol(Protocol, object):
         transport.stopWriting()
 
         socketObject = transport.getHandle()
-        self.factory.mpmManager.sendOutFileDescriptor(socketObject)
+        self.factory.mpmManager.sendOutFileDescriptor(socketObject.fileno())
         socketObject.close()
+
 
 
 class ManagerServerFactory(Factory, object):
@@ -67,10 +76,74 @@ class ManagerOptions(Options, object):
         
         """
         mgr = MPMManager(reactor)
-        endpoint = TCP4ServerEndpoint(reactor)
+        endpoint = TCP4ServerEndpoint(reactor, 8123)
         msf = ManagerServerFactory(mgr)
         yield endpoint.listen(msf)
+        mgr.newSubProcess()
         yield Deferred()
+
+
+class Ping(Command, object):
+    """
+    
+    """
+
+
+class SendDescriptor(Command, object):
+    """
+    
+    """
+    arguments = [("descriptor", Descriptor())]
+    response = []
+    errors = []
+
+
+class ConnectionFromManager(AMP, object):
+    """
+    
+    """
+    def __init__(self, reactor, factory):
+        """
+        
+        """
+        super(ConnectionFromManager, self).__init__()
+        self.factory = factory
+        self.reactor = reactor
+
+
+    def connectionMade(self):
+        """
+        
+        """
+        print("CFM")
+
+
+    def connectionLost(self, reason):
+        """
+        
+        """
+        super(ConnectionFromManager, self).connectionLost(reason)
+        print("CL")
+
+
+    @SendDescriptor.responder
+    def receiveDescriptor(self, descriptor):
+        """
+        
+        """
+        print("receivering")
+        self.reactor.adoptStreamConnection(descriptor, AF_INET, self.factory)
+        print("receivated")
+        return {}
+
+
+    @Ping.responder
+    def pung(self):
+        """
+        
+        """
+        print("received subprocess ping")
+        return {}
 
 
 
@@ -78,7 +151,6 @@ class WorkerOptions(Options, object):
     """
     
     """
-
 
     def postOptions(self):
         """
@@ -91,6 +163,22 @@ class WorkerOptions(Options, object):
         """
         
         """
+        data = Data("Hello world", "text/plain")
+        data.putChild("", data)
+        factory = Site(data)
+
+        # TODO: adoptStreamConnection should really support AF_UNIX
+        protocol = ConnectionFromManager(reactor, factory)
+        fileDescriptor = 7
+        skt = fromfd(fileDescriptor, AF_UNIX, SOCK_STREAM)
+        print("fromfd", skt)
+        # os.close(fileDescriptor)
+        serverTransport = UNIXServer(skt, protocol, None, None, 1234, reactor)
+        print("transported")
+        protocol.makeConnection(serverTransport)
+        print("reading")
+        serverTransport.startReading()
+        print("waiting...")
         return Deferred()
 
 
@@ -117,6 +205,70 @@ class MyProcessProtocol(ProcessProtocol, object):
     """
     mpmManager = attr.ib()
 
+    def outReceived(self, data):
+        """
+        
+        """
+        print("sub-out:", data)
+
+
+    def errReceived(self, data):
+        """
+        
+        """
+        print("sub-err:", data)
+
+
+    def processExited(self, reason):
+        """
+        
+        """
+        print("exit?", reason)
+
+
+    def processEnded(self, reason):
+        """
+        
+        """
+        print("ended?", reason)
+
+
+
+class OneWorkerProtocol(AMP, object):
+    """
+    
+    """
+
+    def connectionMade(self):
+        """
+        
+        """
+        print("OWP_CM")
+
+
+    def connectionLost(self, reason):
+        """
+        
+        """
+        print("OWP_CL", reason)
+        super(OneWorkerProtocol, self).connectionLost(reason)
+
+
+    @inlineCallbacks
+    def sendFD(self, fileDescriptor):
+        """
+        
+        """
+        print("SFD")
+        result1 = yield self.callRemote(Ping)
+        print("sending??????", result1, fileDescriptor)
+        d = self.callRemote(SendDescriptor,
+                            descriptor=fileDescriptor)
+        print("send...ing?", d)
+        result = yield d
+        print("sended/!?@?!", result)
+
+
 
 @attr.s
 class MPMManager(object):
@@ -127,19 +279,28 @@ class MPMManager(object):
     openSubprocessConnections = attr.ib(default=attr.Factory(list))
 
     def sendOutFileDescriptor(self, fileDescriptor):
-        pass
+        if not self.openSubprocessConnections:
+            self.newSubProcess()
+        self.openSubprocessConnections[0].sendFD(fileDescriptor)
 
 
     def newSubProcess(self):
         """
         
         """
-        here, there = socketpair(AF_UNIX, STOCK_STREAM)
-        from twisted.internet.unix import Server as UNIXServer
-        UNIXServer(here, OneWorkerProtocol(), UNIXAddress(None), None, )
-        self.reactor.spawnProcess(
+        here, there = socketpair(AF_UNIX, SOCK_STREAM)
+        owp = OneWorkerProtocol()
+        serverTransport = UNIXServer(here, owp,
+                                     None, None, 4321,
+                                     self.reactor)
+        owp.makeConnection(serverTransport)
+        script = __main__.__file__
+        argv = [sys.executable, script, b'w']
+        print("argv?", argv)
+        procTrans = self.reactor.spawnProcess(
             MyProcessProtocol(self), sys.executable,
-            args=[sys.executable, __main__.__file__], env=os.environ.copy(),
+            args=argv,
+            env=os.environ.copy(),
             childFDs={
                 0: 'w',
                 1: 'r',
@@ -147,7 +308,10 @@ class MPMManager(object):
                 7: there.fileno(),
             }
         )
-
+        print(procTrans)
+        # there.close()
+        serverTransport.startReading()
+        self.openSubprocessConnections.append(owp)
 
 
 @react
@@ -155,7 +319,8 @@ def main(reactor):
     """
     
     """
+    print("BOOTSTRAP")
     clo = CommandLineOptions()
     clo.parseOptions(sys.argv[1:])
-    subCommandParser = clo.subCommand
+    subCommandParser = clo.subOptions
     return subCommandParser.go(reactor)
